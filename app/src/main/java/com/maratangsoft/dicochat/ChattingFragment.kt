4f077b9fbec58355261572407936f345abde1a7c
maratangsoft <maratangsoft@gmail.com>
2022-11-04 17:31:12 +0900

    드로워 대신 OverlappingPanelLayout 적용

--- app/src/main/java/com/maratangsoft/dicochat/ChattingFragment.kt
+++ app/src/main/java/com/maratangsoft/dicochat/ChattingFragment.kt
@@ -2,14 +2,12 @@ package com.maratangsoft.dicochat
 
 import android.os.Bundle
 import android.view.*
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.view.GravityCompat
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import com.maratangsoft.dicochat.databinding.FragmentChattingBinding
 
-class ChattingFragment : Fragment() {
+class ChattingFragment : Fragment(), MenuProvider {
     lateinit var binding: FragmentChattingBinding
 
     override fun onCreateView(
@@ -23,16 +21,27 @@ class ChattingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //왼쪽 드로워 토글버튼 만들기
-        val drawerToggle =ActionBarDrawerToggle(activity, binding.layoutDrawerLeft, binding.toolbar, R.string.drawer_open, R.string.drawer_close)
-        drawerToggle.syncState()
-        binding.layoutDrawerLeft.addDrawerListener(drawerToggle)
+        //메뉴프로바이더 객체 생성
+        requireActivity().addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+        //왼쪽 패널 열기 버튼 (홈버튼) 만들기
+        (activity as MainActivity).setSupportActionBar(binding.panelCentral.toolbar)
+        (activity as MainActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        (activity as MainActivity).supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_hamburger)
+
+
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (binding.layoutDrawerLeft.isDrawerOpen(GravityCompat.START)) binding.layoutDrawerLeft.closeDrawer(GravityCompat.START)
-        binding.layoutDrawerLeft.openDrawer(GravityCompat.END)
-        return super.onOptionsItemSelected(item)
+    //오른쪽 패널 열기 버튼 (옵션메뉴) 만들기
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.fragment_chatting_opt, menu)
+    }
+    //왼쪽/오른쪽 패널 열기 버튼 동작
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId){
+            android.R.id.home -> binding.overlappingPanels.openStartPanel()
+            R.id.opt_drawer_right -> binding.overlappingPanels.openEndPanel()
+        }
+        return true
     }
 
 }
