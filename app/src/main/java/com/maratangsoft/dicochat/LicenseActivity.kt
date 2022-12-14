edeb38890ff878327ac52ed99478ca03427ec2ec
maratangsoft <maratangsoft@gmail.com>
2022-12-14 15:53:41 +0900

    라이선스표기 하다 맘

--- app/src/main/java/com/maratangsoft/dicochat/LicenseActivity.kt
+++ app/src/main/java/com/maratangsoft/dicochat/LicenseActivity.kt
@@ -2,10 +2,13 @@ package com.maratangsoft.dicochat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class LicenseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_license)
+
+        findViewById<Toolbar>(R.id.toolbar).setNavigationOnClickListener { finish() }
     }
 }
